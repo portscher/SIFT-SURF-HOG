f9edd555d0c36ab2ca2694b7f0d73a6c1ff4d347
andrea <andrea.portscher@student.uibk.ac.at>
2021-01-03 19:34:00 +0100

    randomize data sets

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2
 import os.path
+import random
 
 
 def load_image(image_path):
@@ -19,15 +20,20 @@ def load_image(image_path):
 
 def load_set_of_images(folder_name, file_names):
     """
-    loads a whole set of images
+    loads a whole set of images, in random order
     :param folder_name: Name of the folder that contains the images
     :param file_names: List of the files to be loaded
     :return: one list of all training images (80% of all), one list of all testing images (20% of all)
     """
     images = [load_image(os.path.join(folder_name, file_name)) for file_name in file_names]
+    random.shuffle(images)
 
     training_size = int(len(images) * 0.8)
+
+    train_set = images[- training_size:]
+    test_set = images[: - training_size or None]
+
     print("Loaded " + str(len(images)) + " images from " + folder_name + ", " + str(
         training_size) + " for training and " + str(len(images) - training_size) + " for testing.")
 
-    return images[- training_size:], images[: - training_size or None]
+    return train_set, test_set
