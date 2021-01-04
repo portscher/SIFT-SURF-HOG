ff34131dce6e7674ca89d10507177207e663f44b
andrea <andrea.portscher@student.uibk.ac.at>
2021-01-04 21:30:19 +0100

    use SVM from sklearn, add testing and report generation

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 import os.path
 
-import cv2
+from sklearn.metrics import classification_report
+from sklearn.svm import LinearSVC
 
 import feature_extraction
 import utils
+
+import time
 
 
 def main():
     # load images
     train_set, test_set = utils.load_data(os.listdir("img"))
 
+    # extract features from training images
+    print("Using SURF to extract features from the images...")
+    train_descriptors = []
+    train_labels = []
     for (img, lbl) in train_set:
         kp, des = feature_extraction.apply_surf(img)
-        # TODO
+        for d in des:
+            train_descriptors.append(d)
+            train_labels.append(lbl)
 
-    # Train the SVM
-    svm = cv2.ml.SVM_create()
-    svm.setType(cv2.ml.SVM_C_SVC)
-    svm.setKernel(cv2.ml.SVM_LINEAR)
-    svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
+    print("Training the model...")
+    start = time.time()
+    svm = LinearSVC()
+    svm.fit(train_descriptors, train_labels)
+    end = time.time()
+
+    print("Training the model took " + str(end - start) + " seconds.")
+
+    # extract features from test images
+    test_descriptors = []
+    test_labels = []
+
+    for (img, lbl) in test_set:
+        kp, des = feature_extraction.apply_surf(img)
+        for d in des:
+            test_descriptors.append(d)
+            test_labels.append(lbl)
+
+    # test the model and print report
+    predictions = svm.predict(test_descriptors)
+    print(classification_report(test_labels, predictions))
 
 
 if __name__ == "__main__":
