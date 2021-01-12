4497368e935e7126abbcdf6df5e72de52362017f
Juan Pablo <juanolon@gmail.com>
2021-01-12 14:21:03 +0100

    finish sift

--- utils.py
+++ utils.py
@@ -42,3 +42,61 @@ def load_data(folders):
         len(test_set)) + " images for testing. Categories: " + ', '.join(folders))
 
     return train_set, test_set
+
+def load_images(classes):
+    """
+    load all images of the given classes. each class correspond to a folder in the img folder.
+    :return: a list, containing key value pairs (class_id, image)
+    """
+
+    images = []
+    for class_folder in classes:
+        image_set = os.listdir("img/" + class_folder)
+        labelled_images = [(class_folder, load_image("img/" + os.path.join(class_folder, image)))
+                for image in image_set]
+        images.extend(labelled_images)
+
+    print("Loaded " + str(len(images)) + " images of the classes: " + ', '.join(classes))
+
+    return images
+
+def encode_classes(classid):
+    """
+    encode the class-id into an integer
+    :param classid: name of the class
+    :return: an integer identifier for that class
+    """
+
+    val = 0
+    if classid == 'cactus':
+        val = 0
+    if classid == 'dice':
+        val = 1
+    if classid == 'raccoon':
+        val = 2
+    if classid == 'spaghetti':
+        val = 3
+    if classid == 'sushi':
+        val = 4
+
+    return val
+
+def separate_data(images):
+    """
+    :param images: a list tuple (classid, image)
+    :return: X and Y
+    """
+    Y, X = zip(*images)
+
+    Y = map(encode_classes, Y)
+
+    return X, list(Y)
+
+def join_data(X, Y):
+    """
+    :param X: data
+    :param Y: labels
+    :return: list of tuple (Y, X)
+    """
+
+    return zip(Y, X)
